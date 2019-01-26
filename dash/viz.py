f839a5327c616a61c05e3301a9e45cd6fabd326e
D.Lecina <danilecina@gmail.com>
2019-01-26 11:58:53 +0100

    Updated viz tool

--- dash/viz.py
+++ dash/viz.py
@@ -1,8 +1,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 import plotly.graph_objs as go
+import plotly.figure_factory as ff
+import seaborn as sns
+import matplotlib.pyplot as plt
+import dash_table
+
+import pandas as pd
 import numpy as np
 
 from constants import blockNames
@@ -30,11 +35,11 @@ colors = [  '#1f77b4', # muted blue
             '#2ca02c', # cooked asparagus green
             '#d62728', # brick red
             '#9467bd', # muted purple
-            '#8c564b', # chestnut brown
+            '#17becf', # blue-teal
             #'#e377c2', # raspberry yogurt pink
             #'#7f7f7f', # middle gray
             #'#bcbd22', # curry yellow-green
-            '#17becf'] # blue-teal
+            '#8c564b'] # chestnut brown
 runTypesToColors = {runType_order[i]:colors[i] for i in range(len(runType_order))}
 
 available_run_types = [{'value':rt, 'label':blockNames.RUN_TYPES_LONG_NAME_DICTIONARY[rt] } for rt in runType_order]
@@ -50,6 +55,19 @@ available_cols = list(df.columns)
 available_cols.append('week')
 
 time_agg_options = ['day', 'week', 'month', 'year']
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
 
 app.layout = html.Div([
     html.Div([
@@ -125,6 +143,7 @@ app.layout = html.Div([
             dcc.Dropdown(
                 id='yaxis-column2',
                 options=[{'label': i, 'value': i} for i in available_cols],
+                #options=[{'label': i, 'value': i} for i in ['distance']],
                 value='distance'
             ),
             dcc.Checklist(
@@ -143,6 +162,61 @@ app.layout = html.Div([
             )
         ],style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
     dcc.Graph(id='graph2')
+    ], style = {'width' : '49%', 'display': 'inline-block'}),
+    html.Div([
+        dash_table.DataTable(
+                    data=df.to_dict('rows'),
+                    #columns=[{'id': c, 'name': c} for c in df.columns],
+                    columns=[
+                            {"name": ["", "Date"], "id": "date"},
+                            {"name": ["", "Type"], "id": "type"},
+                            {"name": ["", "Dist."], "id": "distance"},
+                            {"name": ["", "Climb"], "id": "climb"},
+                            {"name": ["", "Time"], "id": "time"},
+                            {"name": ["", "Where"], "id": "where"},
+                            {"name": ["", "Notes"], "id": "notes"},
+                            {"name": ["", "Avg. Pace"], "id": "avg_pace"},
+                            {"name": ["Distances", "Dist. E"], "id": "distE"},
+                            {"name": ["Distances", "Dist. M"], "id": "distM"},
+                            {"name": ["Distances", "Dist. T"], "id": "distT"},
+                            {"name": ["Distances", "Dist. I"], "id": "distI"},
+                            {"name": ["Distances", "Dist. R"], "id": "distR"},
+                            {"name": ["Distances", "Dist. X"], "id": "distX"},
+                            {"name": ["Paces", "Pace E"], "id": "paceE"},
+                            {"name": ["Paces", "Pace M"], "id": "paceM"},
+                            {"name": ["Paces", "Pace T"], "id": "paceT"},
+                            {"name": ["Paces", "Pace I"], "id": "paceI"},
+                            {"name": ["Paces", "Pace R"], "id": "paceR"},
+                            {"name": ["Paces", "Pace X"], "id": "paceX"},
+                        ],
+                    style_table={
+                        'overflowX': 'scroll', 
+                        'maxHeight': '300',
+                        'overflowY': 'scroll'},
+                    css=[{
+                            'selector': '.dash-cell div.dash-cell-value',
+                            'rule': 'display: inline; white-space: inherit; overflow: inherit; text-overflow: inherit;'
+                        }],
+                    style_cell={
+                        'whiteSpace': 'no-wrap',
+                        'overflow': 'hidden',
+                        'textOverflow': 'ellipsis',
+                        'minWidth': '70px', 'maxWidth': '100px'
+                    },
+                    #n_fixed_columns=1,
+                    n_fixed_rows=2,
+                    style_as_list_view=True,
+                    merge_duplicate_headers=True,
+                    style_header={
+                            'backgroundColor': 'white',
+                            'fontWeight': 'bold'
+                        },
+                    sorting=True,
+                    sorting_type="multi",
+                ),
+    ]),
+    html.Div([
+        dcc.Graph(id='graph3')
     ], style = {'width' : '49%', 'display': 'inline-block'})
 ])
 
@@ -233,18 +307,29 @@ def update_figure(xaxis_column_name, yaxis_column_name,
     if xaxis_column_name == 'day':
         xaxis_column_name = 'date'
 
-    distance_template = 'dist%s'
-    basicDistances = [distance_template%bt for bt in basicRunning_types]
-    basicDistances.extend(['distance', 'date'])
-    if xaxis_column_name == 'week' or yaxis_column_name == 'week':
-        filt_df = filt_df[:][basicDistances].resample('W', on='date').sum()
-        filt_df['week'] = filt_df.index
-    elif xaxis_column_name == 'month' or yaxis_column_name == 'month':
-        filt_df = filt_df[:][basicDistances].resample('MS', on='date').sum()
-        filt_df['month'] = filt_df.index
-    elif xaxis_column_name == 'year' or yaxis_column_name == 'year':
-        filt_df = filt_df[:][basicDistances].resample('YS', on='date').sum()
-        filt_df['year'] = filt_df.index
+    if yaxis_column_name == 'distance':
+        distance_template = 'dist%s'
+        basicDistances = [distance_template%bt for bt in basicRunning_types]
+        basicDistances.extend(['distance', 'date'])
+        if xaxis_column_name == 'week' or yaxis_column_name == 'week':
+            filt_df = filt_df[:][basicDistances].resample('W', on='date').sum()
+            filt_df['week'] = filt_df.index
+        elif xaxis_column_name == 'month' or yaxis_column_name == 'month':
+            filt_df = filt_df[:][basicDistances].resample('MS', on='date').sum()
+            filt_df['month'] = filt_df.index
+        elif xaxis_column_name == 'year' or yaxis_column_name == 'year':
+            filt_df = filt_df[:][basicDistances].resample('YS', on='date').sum()
+            filt_df['year'] = filt_df.index
+    else:
+        if xaxis_column_name == 'week' or yaxis_column_name == 'week':
+            filt_df = filt_df[:][[yaxis_column_name, 'date']].resample('W', on='date').sum()
+            filt_df['week'] = filt_df.index
+        elif xaxis_column_name == 'month' or yaxis_column_name == 'month':
+            filt_df = filt_df[:][[yaxis_column_name, 'date']].resample('MS', on='date').sum()
+            filt_df['month'] = filt_df.index
+        elif xaxis_column_name == 'year' or yaxis_column_name == 'year':
+            filt_df = filt_df[:][[yaxis_column_name, 'date']].resample('YS', on='date').sum()
+            filt_df['year'] = filt_df.index
 
     selected_basicRunning_types = [t for t in basicRunning_types if t in dropdown_types]
 
@@ -271,7 +356,8 @@ def update_figure(xaxis_column_name, yaxis_column_name,
                         #text=filt_df[:]['date'],
                         #mode='markers',
                         opacity=0.5,
-                        marker={ 'color': runTypesToColors[i] },
+                        #marker={ 'color': runTypesToColors[0] },
+                        marker={ 'color': '#1f77b4' },
                         #marker={ 'size': 15, 'line': {'width': 0.5, 'color': 'white'} },
                         name='All'
                     ) 
@@ -283,11 +369,103 @@ def update_figure(xaxis_column_name, yaxis_column_name,
             barmode='stack',
             xaxis=xaxis_dict,
             yaxis=yaxis_dict,
+            bargap=0.05,
             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
             legend={'x': 0, 'y': 1},
             hovermode='closest'
         )
     }
+
+@app.callback(
+    dash.dependencies.Output('graph3', 'figure'),
+    [dash.dependencies.Input('xaxis-column', 'value'),
+     dash.dependencies.Input('yaxis-column', 'value'),
+     dash.dependencies.Input('type-dropdown', 'value'),
+     dash.dependencies.Input('year--slider', 'value'),
+     dash.dependencies.Input('date-picker-range', 'start_date'),
+     dash.dependencies.Input('date-picker-range', 'end_date')])
+def update_figure(xaxis_column_name, yaxis_column_name,
+                 selected_types, chosen_year,
+                 start_date, end_date):
+
+    if chosen_year == df.date.dt.year.max()+1:
+        filt_df = df
+    else:
+        filt_df = df[df.date.dt.year == chosen_year]
+
+    filt_df = filt_df[np.logical_and(filt_df.date >= start_date, filt_df.date <= end_date)]
+
+    data = [
+        go.Histogram2dContour(
+            x=filt_df[filt_df['type'].isin(selected_types)][xaxis_column_name],
+            y=filt_df[filt_df['type'].isin(selected_types)][yaxis_column_name],
+            colorscale = 'Blues',
+            reversescale = True,
+            xaxis = 'x',
+            yaxis = 'y',
+            nbinsx=25,
+            nbinsy=25,
+            ncontours=20,
+            contours = dict(coloring="fill"),
+            name='Density',
+            histnorm="density"
+        ),
+        go.Histogram(
+            y=filt_df[filt_df['type'].isin(selected_types)][yaxis_column_name],
+            xaxis = 'x2',
+            marker = dict( color = 'rgba(0,0,0,1)'),
+            name="Histogram: %s"%yaxis_column_name
+        ),
+        go.Histogram(
+            x=filt_df[filt_df['type'].isin(selected_types)][xaxis_column_name],
+            yaxis = 'y2',
+            marker = dict( color = 'rgba(0,0,0,1)'),
+            name="Histogram: %s"%xaxis_column_name
+        )
+    ]
+
+    scatter_traces = [
+                go.Scatter(
+                    x=filt_df[filt_df['type'] == i][xaxis_column_name],
+                    y=filt_df[filt_df['type'] == i][yaxis_column_name],
+                    text=filt_df[filt_df['type'] == i]['date'],
+                    mode='markers',
+                    opacity=0.5,
+                    marker={
+                        'size': 5,
+                        'line': {'width': 0.5, 'color': 'white'},
+                        'color': runTypesToColors[i]
+                    },
+                    xaxis = 'x',
+                    yaxis = 'y',
+                    name=i
+                ) for i in selected_types 
+    ]
+    data.extend(scatter_traces)
+
+    layout = go.Layout(
+        autosize = True,
+        xaxis = dict( zeroline = False, domain = [0,0.85], showgrid = False, title = xaxis_column_name),
+        yaxis = dict( zeroline = False, domain = [0,0.85], showgrid = False, title = yaxis_column_name),
+        xaxis2 = dict( zeroline = False, domain = [0.85,1], showgrid = False),
+        yaxis2 = dict( zeroline = False, domain = [0.85,1], showgrid = False),
+        height = 600,
+        width = 600,
+        bargap = 0,
+        hovermode = 'closest',
+        showlegend = False
+    )
+
+    fig = go.Figure(data=data,layout=layout)
+
+    ##########
+    x=filt_df[filt_df['type'] == i][xaxis_column_name]
+    y=filt_df[filt_df['type'] == i][yaxis_column_name]
+    fig = ff.create_2d_density(
+        x, y,
+        hist_color='rgb(255, 237, 222)', point_size=3
+    )
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)

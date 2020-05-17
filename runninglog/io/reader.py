f2ca105f6d2e7161a6e496581fb04648bb32c724
D.Lecina <danilecina@gmail.com>
2020-05-17 17:42:25 +0200

    Style changes

--- runninglog/io/reader.py
+++ runninglog/io/reader.py
@@ -5,18 +5,20 @@ import sys
 
 from runninglog.constants import constants
 
+
 def __unicodeToStr(data):
-    #convert dict
+    # convert dict
     if isinstance(data, dict):
-        return { __unicodeToStr(key): __unicodeToStr(value) for key, value in data.iteritems() }
-    #convert list
+        return {__unicodeToStr(key): __unicodeToStr(value) for key, value in data.iteritems()}
+    # convert list
     if isinstance(data, list):
-        return [ __unicodeToStr(val) for val in data ]
-    #convert unicode to str
+        return [__unicodeToStr(val) for val in data]
+    # convert unicode to str
     if isinstance(data, unicode):
         return data.encode('utf-8')
 
     return data
+
 
 def get_files_in_subdirs(directory, extension):
     """Get all files in dir and subdirs that match extension
@@ -34,6 +36,7 @@ def get_files_in_subdirs(directory, extension):
         for filename in fnmatch.filter(filenames, extension):
             file_list.append(os.path.join(root, filename))
     return file_list
+
 
 def read_json_file(filename):
     """Read JSON file
@@ -54,8 +57,6 @@ def read_json_file(filename):
         except json.JSONDecodeError as err:
             raise Exception(f"Could not read: {json_filename}; "
                             f"Error: {err}") from err
-        except:
-            raise Exception(f"Could not read: {json_filename}") from err
 
     return parsed_json
 
@@ -66,11 +67,11 @@ def get_json_runs_in_subdirs(directory, verbose=False):
     read_runs = []
     for filename in file_list:
         if verbose:
-            print ("Reading", filename)
+            print("Reading", filename)
 
         parsed_json = read_json_file(filename)
 
-        #Omit empty JSON or files
+        # Omit empty JSON or files
         if parsed_json != constants.EMPTY_JSON and parsed_json != "":
             read_runs.append(parsed_json)
 

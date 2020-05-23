f2965537b0fe1a8130b406042398a8a8fbc4454a
D.Lecina <danilecina@gmail.com>
2020-05-23 20:03:32 +0200

    Improved style

--- main.py
+++ main.py
@@ -4,40 +4,46 @@ from runninglog.runninglog import runninglog
 from runninglog.config import config
 from runninglog.utilities import utilities
 
+
 def parseArgs():
     desc_str = "Running log program.\n"\
                 "Processes JSON files with running events and generates\n"\
                 "output for further exploitation"
     parser = argparse.ArgumentParser(description=desc_str)
 
-    parser.add_argument("-t", "--template", action='store_true', help="Generate template file")
-    parser.add_argument("-f", "--force", action='store_true', help="Force reading previous runs")
-    parser.add_argument("-c", "--config_file", type=str, default="", help="Config file path")
+    parser.add_argument("-t", "--template", action='store_true',
+                        help="Generate template file")
+    parser.add_argument("-f", "--force", action='store_true',
+                        help="Force reading previous runs")
+    parser.add_argument("-c", "--config_file", type=str, default="",
+                        help="Config file path")
 
     args = parser.parse_args()
     return args
 
+
 def generate_empty_json(self, config):
     fname_template = os.path.join(config.input_dir,
-                                constants.JSON_TEMPLATE_OUTPUT_NAME)
-    fname = fname_template%"" 
-    i=1
+                                  constants.JSON_TEMPLATE_OUTPUT_NAME)
+    fname = fname_template % ""
+    i = 1
     while os.path.isfile(fname):
-        suffix = "_%d"%i
-        fname = fname_template%suffix
+        suffix = "_%d" % i
+        fname = fname_template % suffix
         i += 1
-        
+
     utilities.make_dir(config.input_dir)
     with open(fname, "w") as json_file:
         json_file.write(constants.EMPTY_JSON)
 
-    print ("Generated: %s"%fname)
+    print(f"Generated: {fname}")
+
 
 def main():
     args = parseArgs()
 
     if args.template:
-        print ("Generating template\n")
+        print("Generating template\n")
         generate_empty_json()
         return
 
@@ -51,13 +57,14 @@ def main():
     else:
         utilities.cleanup(configuration.output_dir)
 
-    print (f"Reading runs from {configuration.input_dir}")
+    print(f"Reading runs from {configuration.input_dir}")
     rl.load_runs()
-    print ("Computing embedding")
+    print("Computing embedding")
     rl.compute_embedding()
-    print ("Finished!")
+    print("Finished!")
 
     rl.save()
+
 
 if __name__ == "__main__":
     main()
